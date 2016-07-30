c20756795e1cc286f48a8a32f22017b7d7160416
Chris Gorgolewski <krzysztof.gorgolewski@gmail.com>
2016-07-30 11:01:32 -0700

    spec refactor

--- README.md
+++ README.md
@@ -9,40 +9,48 @@ Apps command line arguments.
 
 This App has the following comman line arguments:
 
-    usage: run.py [-h]
-                  [--participant_label PARTICIPANT_LABEL [PARTICIPANT_LABEL ...] |
-                  --first_level_results FIRST_LEVEL_RESULTS]
-                  bids_dir output_dir
+		usage: run.py [-h]
+		              [--participant_label PARTICIPANT_LABEL [PARTICIPANT_LABEL ...]]
+		              bids_dir output_dir {participant,group}
 
-    Example BIDS App entrypoint script.
+		Example BIDS App entrypoint script.
 
-    positional arguments:
-      bids_dir              The directory with the input dataset formatted
-                            according to the BIDS standard.
-      output_dir            The directory where the output files should be stored.
+		positional arguments:
+		  bids_dir              The directory with the input dataset formatted
+		                        according to the BIDS standard.
+		  output_dir            The directory where the output files should be stored.
+		                        If you are running group level analysis this folder
+		                        should be prepopulated with the results of
+		                        theparticipant level analysis.
+		  {participant,group}   Level of the analysis that will be performed. Multiple
+		                        participant level analyses can be run independently
+		                        (in parallel).
 
-    optional arguments:
-      -h, --help            show this help message and exit
-      --participant_label PARTICIPANT_LABEL [PARTICIPANT_LABEL ...]
-                            (optional - first level only) label of the participant
-                            that should be analyzed. The label corresponds to
-                            sub-<participant_label> from the BIDS spec (so it does
-                            not include "sub-"). If this parameter is not provided
-                            all subjects should be analyzed. Multiple participants
-                            can be specified with a space separated list.
-      --first_level_results FIRST_LEVEL_RESULTS
-                            (group level only) directory with outputs from a set
-                            of participants which will be used as input to the
-                            group level (this is where first level pipelines store
-                            outputs via output_dir).
+		optional arguments:
+		  -h, --help            show this help message and exit
+		  --participant_label PARTICIPANT_LABEL [PARTICIPANT_LABEL ...]
+		                        The label(s) of the participant(s) that should be
+		                        analyzed. The label corresponds to
+		                        sub-<participant_label> from the BIDS spec (so it does
+		                        not include "sub-"). If this parameter is not provided
+		                        all subjects should be analyzed. Multiple participants
+		                        can be specified with a space separated list.
 
-To run it in first level mode (for one participant):
+To run it in participant level mode (for one participant):
 
-    docker run -i --rm -v /Users/filo/data/ds005:/bids_dataset -v /Users/filo/first_level_results:/first_level_results bids/example /bids_dataset /first_level_results --participant_label 01
+    docker run -i --rm \
+		-v /Users/filo/data/ds005:/bids_dataset \
+		-v /Users/filo/outputs:/outputs \
+		bids/example \
+		/bids_dataset /outputs participant --participant_label 01
 
 After doing this for all subjects (potentially in parallel) the group level analysis
 can be run:
 
-    docker run -i --rm -v /Users/filo/data/ds005:/bids_dataset -v /Users/filo/first_level_results:/first_level_results -v /Users/filo/group_level_results:/group_level_results bids/example /bids_dataset /group_level_results --first_level_results /first_level_results
+    docker run -i --rm \
+		-v /Users/filo/data/ds005:/bids_dataset \
+		-v /Users/filo/outputs:/outputs \
+		bids/example \
+		/bids_dataset /outputs group
 
 For more information about the specification of BIDS Apps see [here](https://docs.google.com/document/d/1E1Wi5ONvOVVnGhj21S1bmJJ4kyHFT7tkxnV3C23sjIE/edit#).
--- run.py
+++ run.py
@@ -8,52 +8,53 @@ from glob import glob
 
 parser = argparse.ArgumentParser(description='Example BIDS App entrypoint script.')
 parser.add_argument('bids_dir', help='The directory with the input dataset '
-                    'formatted according to the BIDS standard.')
+					'formatted according to the BIDS standard.')
 parser.add_argument('output_dir', help='The directory where the output files '
-                    'should be stored.')
-group = parser.add_mutually_exclusive_group()
-group.add_argument('--participant_label', help='The label of the participant that should be analyzed. The label '
+					'should be stored. If you are running group level analysis '
+					'this folder should be prepopulated with the results of the'
+					'participant level analysis.')
+parser.add_argument('analysis_level', help='Level of the analysis that will be performed. '
+					'Multiple participant level analyses can be run independently '
+					'(in parallel) using the same output_dir.',
+					choices=['participant', 'group'])
+parser.add_argument('--participant_label', help='The label(s) of the participant(s) that should be analyzed. The label '
 				   'corresponds to sub-<participant_label> from the BIDS spec '
 				   '(so it does not include "sub-"). If this parameter is not '
 				   'provided all subjects should be analyzed. Multiple '
 				   'participants can be specified with a space separated list.',
-                   nargs="+")
-group.add_argument('--first_level_results', help='(group level only) directory with outputs '
-                    'from a set of participants which will be used as input to '
-                    'the group level (this is where first level pipelines store '
-                    'outputs via output_dir).')
+				   nargs="+")
 
 args = parser.parse_args()
 
 subjects_to_analyze = []
 # only for a subset of subjects
 if args.participant_label:
-    subjects_to_analyze = args.participant_label
+	subjects_to_analyze = args.participant_label
 # for all subjects
 else:
-    subject_dirs = glob(os.path.join(args.bids_dir, "sub-*"))
-    subjects_to_analyze = [subject_dir.split("-")[-1] for subject_dir in subject_dirs]
+	subject_dirs = glob(os.path.join(args.bids_dir, "sub-*"))
+	subjects_to_analyze = [subject_dir.split("-")[-1] for subject_dir in subject_dirs]
 
 # running first level
-if not args.first_level_results:
+if args.analysis_level == "participant":
 
-    # find all T1s and skullstrip them
-    for subject_label in subjects_to_analyze:
-        for T1_file in glob(os.path.join(args.bids_dir, "sub-%s"%subject_label,
-                                         "anat", "*_T1w.nii*")) + glob(os.path.join(args.bids_dir,"sub-%s"%subject_label,"ses-*","anat", "*_T1w.nii*")):
-            out_file = os.path.split(T1_file)[-1].replace("_T1w.", "_brain.")
-            cmd = "bet %s %s"%(T1_file, os.path.join(args.output_dir, out_file))
-            print(cmd)
-            subprocess.run(cmd, shell=True, check=True)
+	# find all T1s and skullstrip them
+	for subject_label in subjects_to_analyze:
+		for T1_file in glob(os.path.join(args.bids_dir, "sub-%s"%subject_label,
+										 "anat", "*_T1w.nii*")) + glob(os.path.join(args.bids_dir,"sub-%s"%subject_label,"ses-*","anat", "*_T1w.nii*")):
+			out_file = os.path.split(T1_file)[-1].replace("_T1w.", "_brain.")
+			cmd = "bet %s %s"%(T1_file, os.path.join(args.output_dir, out_file))
+			print(cmd)
+			subprocess.run(cmd, shell=True, check=True)
 
 # running group level
-else:
-    brain_sizes = []
+elif args.analysis_level == "group":
+	brain_sizes = []
 	for subject_label in subjects_to_analyze:
-	    for brain_file in glob(os.path.join(args.first_level_results, "sub-%s*.nii*"%subject_label)):
-	        data = nibabel.load(brain_file).get_data()
-	        # calcualte average mask size in voxels
-	        brain_sizes.append((data != 0).sum())
+		for brain_file in glob(os.path.join(args.output_dir, "sub-%s*.nii*"%subject_label)):
+			data = nibabel.load(brain_file).get_data()
+			# calcualte average mask size in voxels
+			brain_sizes.append((data != 0).sum())
 
-    with open(os.path.join(args.output_dir, "avg_brain_size.txt"), 'w') as fp:
-        fp.write("Average brain size is %g voxels"%numpy.array(brain_sizes).mean())
+	with open(os.path.join(args.output_dir, "avg_brain_size.txt"), 'w') as fp:
+		fp.write("Average brain size is %g voxels"%numpy.array(brain_sizes).mean())
