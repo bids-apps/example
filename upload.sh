5d132587c01f84f3306554c083bfc649d9c6397a
Chris Gorgolewski <krzysztof.gorgolewski@gmail.com>
2016-08-07 19:55:05 -0700

    fixed binary name

--- upload.sh
+++ upload.sh
@@ -5,17 +5,17 @@ set -o nounset
 app_name=$1
 refresh_token=$2
 file_name=$3
-bids_apps_dir_id=`./gdrive-windows-x64.exe list -q "name = 'bids-apps' and trashed = false and mimeType = 'application/vnd.google-apps.folder'" --refresh-token $refresh_token --no-header | cut -f1 -d " "`
+bids_apps_dir_id=`./gdrive list -q "name = 'bids-apps' and trashed = false and mimeType = 'application/vnd.google-apps.folder'" --refresh-token $refresh_token --no-header | cut -f1 -d " "`
 echo $bids_apps_dir_id
 
-app_dir_id=`./gdrive-windows-x64.exe list -q "name = '$app_name' and '$bids_apps_dir_id' in parents and trashed = false and mimeType = 'application/vnd.google-apps.folder'" --refresh-token $refresh_token --no-header | cut -f1 -d " "`
+app_dir_id=`./gdrive list -q "name = '$app_name' and '$bids_apps_dir_id' in parents and trashed = false and mimeType = 'application/vnd.google-apps.folder'" --refresh-token $refresh_token --no-header | cut -f1 -d " "`
 echo $app_dir_id
 if [ -z "$app_dir_id" ]
 then
-  ./gdrive-windows-x64.exe mkdir "$app_name" -p "$bids_apps_dir_id" --refresh-token $refresh_token
-	app_dir_id=`./gdrive-windows-x64.exe list -q "name = '$app_name' and '$bids_apps_dir_id' in parents and trashed = false and mimeType = 'application/vnd.google-apps.folder'" --refresh-token $refresh_token --no-header | cut -f1 -d " "`
+  ./gdrive mkdir "$app_name" -p "$bids_apps_dir_id" --refresh-token $refresh_token
+	app_dir_id=`./gdrive list -q "name = '$app_name' and '$bids_apps_dir_id' in parents and trashed = false and mimeType = 'application/vnd.google-apps.folder'" --refresh-token $refresh_token --no-header | cut -f1 -d " "`
 fi
 
 echo $app_dir_id
 
-./gdrive-windows-x64.exe upload $file_name -p $app_dir_id --refresh-token $refresh_token
+./gdrive upload $file_name -p $app_dir_id --refresh-token $refresh_token
